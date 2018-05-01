3f0435ee7cac16c573bd6396effc0a077a013dd9
diazMafer <31084398+diazMafer@users.noreply.github.com>
2018-05-01 14:50:47 -0600

    pruebas inservibles

---
+++ Cactus Juice/Assets/Scripts/CactusTap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CactusTap : MonoBehaviour {
+    Animator anim;
+    Touch touch;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        anim.SetBool("Speed", true);
+        if (Input.touchCount > 0)
+        {
+            Debug.Log(Input.GetTouch(0).position);
+        }
+        
+	}
+}
+
